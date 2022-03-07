807c2a337cf34b3e580212b46575d0f0d042c24d
Clemens Akens <clebert@me.com>
2022-03-07 01:49:36 +0100

    Update @onecfg/standard

--- onecfg.js
+++ onecfg.js
@@ -5,6 +5,7 @@ import {
   eslint,
   git,
   github,
+  ignore,
   javascript,
   jest,
   node,
@@ -14,16 +15,19 @@ import {
   typescript,
   vscode,
 } from '@onecfg/standard';
-import {mergeContent, onecfg} from './lib/index.js';
+import {onecfg} from './lib/index.js';
 
 onecfg(
   ...editorconfig(),
   ...eslint(),
   ...git(),
   ...github(),
+  ...ignore(`test`),
+
   ...javascript({
     target: {ecmaVersion: `es2021`, moduleType: `es2020`, node: true},
   }),
+
   ...jest(),
   ...node({nodeVersion: `16`}),
   ...npm(),
@@ -31,7 +35,4 @@ onecfg(
   ...swc(),
   ...typescript({declaration: true, outDir: `lib`, sourceMap: true}),
   ...vscode({includeAllFiles: false}),
-
-  mergeContent(git.ignoreFile, [`test`]),
-  mergeContent(vscode.settingsFile, {'files.exclude': {test: true}}),
 );
